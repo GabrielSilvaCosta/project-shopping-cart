b2d25819cb2dd109c727b2f6a154cf37402e0077
GabrielSilvaCosta <bob@>
2023-03-22 19:53:43 -0300

    requisito 3 feito

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { searchCep } from './helpers/cepFunctions';
+import { createProductElement } from './helpers/shopFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+// chamamos a função 'fetchProductsList' passando o parametro computador
+const products = await fetchProductsList('computador');
+
+// criei um elemento html para cada produto utilizando a função
+// createProductElement(), com o forEach para cada elemento e adicionamo
+const productsSection = document.querySelector('.products');
+products.forEach((product) => {
+  const productElement = createProductElement(product);
+  productsSection.appendChild(productElement);
+});
